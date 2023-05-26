7bb184f1064478912883106b12668cf282b8abcd
Cyrille Varin <cyrille.varin@live.fr>
2023-05-26 23:13:29 +0200

    rect bug3

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from PIL import Image
 import io
 
 '''
-# Image convertisseur front v0.0.4
+# Image convertisseur front v0.0.5
 '''
 
 def convert_image(input_path, output_format):
@@ -14,23 +14,17 @@ def convert_image(input_path, output_format):
         im.save(buffer, format=output_format)
         return buffer.getvalue()
 
-with st.form('params_for_api'):
-
-    pickup_format = st.selectbox('Choose your format',
+pickup_format = st.selectbox('Choose your format',
     ("JPEG", "PNG", "GIF", "BMP", "TIFF", "WebP", "ICO", "PBM", "PGM", "PPM", "RGB", "RGBA", "CMYK", "YCbCr", "LAB", "HSV", "I;16", "I;16L", "I;16B", "F"))
 
-    take_file = st.file_uploader('Image uploader')
+take_file = st.file_uploader('Image uploader')
 
-    if take_file:
-
+if take_file:
+    if st.button('Convertir mon image'):
         transformed_img = convert_image(take_file, pickup_format)
-
-        st.image(transformed_img, use_column_width=True)
-
-        if st.form_submit_button('Convertir mon image'):
-            st.download_button(
-                label="Download the converted image",
-                data=transformed_img,
-                file_name="converted_image.jpg",
-                mime="image/jpeg"
-            )
+        st.download_button(
+            label="Download the converted image",
+            data=transformed_img,
+            file_name="converted_image.jpg",
+            mime="image/jpeg"
+        )
