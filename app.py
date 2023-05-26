c57c4ec89b2495a9c1f69c38739869b7c9207dfd
Cyrille Varin <cyrille.varin@live.fr>
2023-05-26 23:06:45 +0200

    fonctionnalité de telechargement

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ import io
 # Image convertisseur front v0.0.2
 '''
 
-def convert_image(input_path, output_format):
-    with Image.open(input_path) as im:
+def convert_image(input_file, output_format):
+    with Image.open(input_file) as im:
         buffer = io.BytesIO()
         im.save(buffer, format=output_format)
         return buffer.getvalue()
@@ -23,14 +23,19 @@ with st.form(key='params_for_api'):
 
     if take_file:
 
-        transformed_img = convert_image(take_file, pickup_format)
+        transformed_img = convert_image(take_file.getvalue(), pickup_format)
 
         st.image(transformed_img, use_column_width=True)
 
-        download_button = st.download_button(
+        download_button = st.button(
             label="Download the converted image",
             data=transformed_img,
             file_name="converted_image"
         )
 
-    st.form_submit_button('Convertir mon image')
+if download_button:
+    st.download_button(
+        data=transformed_img,
+        file_name="converted_image",
+        mime="image/{}".format(pickup_format.lower())  # Format the MIME type with proper extension
+    )
